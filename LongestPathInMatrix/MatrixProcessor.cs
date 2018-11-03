5274e517fd292675667b118a85ea182e734b6ca9
gibeulah <gibeulah@microsoft.com>
2018-11-03 16:19:55 +0530

    First Commit

---
+++ LongestPathInMatrix/MatrixProcessor.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LongestPathInMatrix
+{
+    class MatrixProcessor
+    {
+        private int[,] matrix;
+
+        public MatrixProcessor(int[,] inputMatrix) {
+            matrix = inputMatrix;
+        }
+
+        private void PrintMatrix(int[,] matrix) {
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+
+            Console.WriteLine("The contents of the matrix are ................");
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    Console.Write(matrix[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("................................");
+        }
+
+        public void GetLongestPathInMatrix() {
+
+            Console.WriteLine("The input matrix.................");
+            PrintMatrix(this.matrix);
+
+            int longestPath = int.MinValue;
+            int rows = this.matrix.GetLength(0);
+            int columns = this.matrix.GetLength(1);
+
+            int[,] table = new int[rows, columns];
+
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    if (table[i, j] == 0) {
+                        table[i, j] = FindLongestPathFromThisCell(i, j, table);
+                    }
+
+                    if (table[i, j] > longestPath) {
+                        longestPath = table[i,j];
+                    }
+                }
+            }
+            Console.WriteLine("The constructed DP table matrix will be displayed....");
+            PrintMatrix(table);
+            Console.WriteLine("The longest Path in the matrix is "+longestPath);
+        }
+
+        private int FindLongestPathFromThisCell(int i, int j, int[,] table) {
+
+            int rows = this.matrix.GetLength(0);
+            int columns = this.matrix.GetLength(1);
+
+            if (i >= rows || j >= columns || i < 0 || j < 0) {
+                return 0;
+            }
+
+            if (table[i, j] != 0) {
+                return table[i, j];
+            }
+
+            //The actual processing - Make sure to return when required for recursions!!!
+
+            //Right Navigation
+            if (j + 1 < columns && (this.matrix[i, j] + 1) == this.matrix[i, j + 1]) {
+                return table[i, j] = 1 + FindLongestPathFromThisCell(i, j+1, table);
+            }
+
+            //Left Navigation
+            if (j > 0 && (this.matrix[i, j] + 1) == this.matrix[i, j - 1]){
+                return table[i, j] = 1 + FindLongestPathFromThisCell(i, j - 1, table);
+            }
+
+            //Top Navigation
+            if (i > 0 && (this.matrix[i, j] + 1) == this.matrix[i-1, j]){
+                return table[i, j] = 1 + FindLongestPathFromThisCell(i-1, j, table);
+            }
+
+            //Bottom Navigation
+            if (i+1 < rows && (this.matrix[i, j] + 1) == this.matrix[i + 1, j])
+            {
+                return table[i, j] = 1 + FindLongestPathFromThisCell(i + 1, j, table);
+            }
+
+            return table[i, j] = 1;
+        }
+
+    }
+}
--- LongestPathInMatrix/Program.cs
+++ LongestPathInMatrix/Program.cs
@@ -6,7 +6,56 @@ namespace LongestPathInMatrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Maximum path in a matrix!");
+            Console.WriteLine("--------------------------");
+
+            Console.WriteLine("Four directions can be used, left, right, top, bottom");
+            Console.WriteLine("Adjacent cell can be navigated given that its number is one greater than the current number");
+
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------------------------------------------------------------");
+            try
+            {
+                int[,] matrix = GetInput();
+                if (matrix != null)
+                {
+                    MatrixProcessor matrixProcessor = new MatrixProcessor(matrix);
+                    matrixProcessor.GetLongestPathInMatrix();
+
+                    Console.ReadKey();
+                }
+                else {
+                    Console.WriteLine("Matrix is null! Please check your inputs and try again!");
+                    Console.WriteLine("Terminating.......");
+                }
+            }
+            catch (Exception exception) {
+                Console.WriteLine("Exception thrown is "+exception.Message);
+            }
+        }
+
+        private static int[,] GetInput() {
+            int[,] inputMatrix = null;
+            Console.WriteLine("Enter the number of rows in the matrix");
+            try
+            {
+                int noRows = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the number of columns in the matrix");
+                int noColumns = int.Parse(Console.ReadLine());
+                inputMatrix = new int[noRows,noColumns];
+
+                for (int i = 0; i < noRows; i++) {
+                    Console.WriteLine("Enter the elements of row:"+i);
+                    for (int j = 0; j < noColumns; j++) {
+                        Console.WriteLine("Enter the element:");
+                        inputMatrix[i, j] = int.Parse(Console.ReadLine());
+                    }
+                }
+            }
+            catch (Exception exception) {
+                Console.WriteLine("Thrown exception is"+exception.Message);
+            }
+            return inputMatrix;
         }
     }
 }
